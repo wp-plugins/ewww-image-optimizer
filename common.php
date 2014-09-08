0accd9656b07296c6284cd8735333bf9d1f5d817
nosilver4u <nosilver4u@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-08 22:09:28 +0000

    missed some version number updates
    git-svn-id: https://plugins.svn.wordpress.org/ewww-image-optimizer/trunk@984754 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- common.php
+++ common.php
@@ -1,7 +1,7 @@
 <?php
 // common functions for Standard and Cloud plugins
 // TODO: check all comments to make sure they are actually useful...
-define('EWWW_IMAGE_OPTIMIZER_VERSION', '200.2');
+define('EWWW_IMAGE_OPTIMIZER_VERSION', '201');
 
 // initialize debug global
 $disabled = ini_get('disable_functions');
